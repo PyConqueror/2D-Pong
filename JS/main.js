1ff97a2091ea0bdc83da2a1d0a5d960e27450697
Wan Aqim <wanaqim@gmail.com>
2023-10-12 15:36:30 +0800

    Added Pseudocode

---
+++ JS/main.js
@@ -0,0 +1,29 @@
+// Start :
+// start message/prompt : Click Start to play
+
+
+// Set ball to center of the board
+// Set paddle_1 (human) to right side and vertically centered
+// Set paddle_2 (AI) to left side and vertically centered
+// Set initial ball direction (left or right)
+// Set initial scores to 0 for both human and AI
+// when ball hit the paddle, play bounce sound
+// Score limit of 10
+
+// if player reach score of 10, prompt user wins message
+// otherwise prompt user lose message
+
+// game board logic :
+// ball cannot exit top or bottom of board - reverse ball direction
+// if ball collides with left or right paddle, - reverse ball direction
+// if ball passes left paddle = increase player socre by 1 
+// if ball passes right paddle = increase AI score by 1
+
+// game controls:
+// up / down button to control player's paddle (left paddle)
+// create a AI algo to follow ball's vertical position (right paddle)
+
+
+
+
+
