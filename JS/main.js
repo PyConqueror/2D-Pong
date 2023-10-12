c1768a9b87152b81957dd0e2a1397860842a8f83
Wan Aqim <wanaqim@gmail.com>
2023-10-12 16:02:20 +0800

    final pseudocode adjustments

--- JS/main.js
+++ JS/main.js
@@ -4,10 +4,10 @@
 
 // Set ball to center of the board
 // Set paddle_1 (AI) to left side and centered (centered vertically in board)
-// Set paddle_2 (human) to right side and centered (centered vertically in board)
-// Set initial ball direction (left or right)
+// Set paddle_2 (Player) to right side and centered (centered vertically in board)
+// Set initial ball direction (center to left or right)
 // Set initial scores to 0 for both human and AI
-// when ball hit the paddle, play bounce sound
+
 // Score limit of 10
 
 // if player reach score of 10, prompt user wins message
@@ -16,12 +16,13 @@
 // game board logic :
 // ball cannot exit top or bottom of board - reverse ball direction
 // if ball collides with left or right paddle, - reverse ball direction
+// when ball hit the paddle, play bounce sound
 // if ball passes left paddle = increase player socre by 1 
 // if ball passes right paddle = increase AI score by 1
 
 // game controls:
-// up / down button to control player's paddle (left paddle)
-// create a AI algo to follow ball's vertical position (right paddle)
+// up / down button to control player's paddle (right paddle)
+// create a AI algo to follow ball's vertical position (left paddle)
 
 /*----- constants -----*/
 
