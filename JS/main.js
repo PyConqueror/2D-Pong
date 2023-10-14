759648dce15a38a9d1e62c26d377142d3bb19fce
Wan Aqim <wanaqim@gmail.com>
2023-10-14 11:54:56 +0800

    add init state

--- JS/main.js
+++ JS/main.js
@@ -3,8 +3,8 @@
 
 
 // Set ball to center of the board
-// Set paddle_1 (AI) to left side and centered (centered vertically in board)
-// Set paddle_2 (Player) to right side and centered (centered vertically in board)
+// Set left_paddle (AI) to left side and centered (centered vertically in board)
+// Set right_paddle (Player) to right side and centered (centered vertically in board)
 // Set initial ball direction (center to left or right)
 // Set initial scores to 0 for both human and AI
 
@@ -15,7 +15,7 @@
 
 // game board logic :
 // ball cannot exit top or bottom of board - reverse ball direction
-// if ball collides with left or right paddle, - reverse ball direction
+// if ball hit with left or right paddle, - reverse ball direction
 // when ball hit the paddle, play bounce sound
 // if ball passes left paddle = increase player socre by 1 
 // if ball passes right paddle = increase AI score by 1
@@ -35,20 +35,35 @@ let winner
 /*----- cached element references -----*/
 const player_score = document.querySelector('.player_score')
 const AI_score = document.querySelector('.AI_score')
-const left_paddle = document.querySelector('.paddle_1')
-const right_paddle = document.querySelector('.paddle_2')
+const left_paddle = document.querySelector('.left_paddle')
+const right_paddle = document.querySelector('.right_paddle')
 const ball = document.querySelector('.ball')
+const play_button = document.querySelector('#start_button')
 /*----- event listeners -----*/
-
+play_button.addEventListener('click, init')
 
 /*----- functions -----*/
 init()
 
 function init() {
-    scores = {
+    scores = { //upon calling, reset the scores to 0 
         Player : 0,
         AI : 0
     }
+    reset_board() //reset the board to the inital state
+    update_score() //update the score after resetting
+}
+
+function reset_board() {
+    ball.style.top =  '50%' //reset the ball to middle of the board
+    ball.style.left = '50%' 
+    left_paddle.style.top = '50%' //reset the paddle to middle of the board
+    right_paddle.style.top = '50%'
+}
+
+function update_score() {
+    player_score.innerText = scores.Player  
+    AI_score.innerText = scores.AI
 }
 
 
