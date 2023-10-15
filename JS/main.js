218158ac434fda652aae727edce0a114c8548ce7
Wan Aqim <wanaqim@gmail.com>
2023-10-15 16:28:10 +0800

    add paddle collision logic

--- JS/main.js
+++ JS/main.js
@@ -35,8 +35,8 @@ let winner
 let ball_position = {
     x_axis: 300, // x-axis 300 (half of 600px width)
     y_axis: 200, // y-axis 200(half of 400px height)
-    x_velocity: 2, // Speed and direction of the ball on the x axis
-    y_velocity: 2  // Speed and direction of the ball on the y axis
+    x_velocity: 5, // Speed and direction of the ball on the x axis
+    y_velocity: 5  // Speed and direction of the ball on the y axis
 }
 
 /*----- cached element references -----*/
@@ -50,7 +50,7 @@ const countdown = document.querySelector('.countdown')
 
 /*----- event listeners -----*/
 play_button.addEventListener('click', init)
-document.addEventListener('keydown', move_paddle);
+document.addEventListener('keydown', move_paddle)
 
 /*----- functions -----*/
 
@@ -75,10 +75,10 @@ function reset_board() {
 }
 
 function reset_ball_position() {
-    ball_position.x_axis = 300;
-    ball_position.y_axis = 200;
-    ball_position.x_velocity = 2;
-    ball_position.y_velocity = 2;
+    ball_position.x_axis = 300
+    ball_position.y_axis = 200
+    ball_position.x_velocity = 2
+    ball_position.y_velocity = 2
 }
 
 function update_score() {
@@ -109,6 +109,7 @@ function start_countdown() {
 function render() {
     if (game_status === 1) { //check if game is running, if yes render everything.
         ball.style.display = 'block'//make ball visible  
+        ball_collision_with_paddle()
         update_ball_heading()
         ball_collision()
         render_ball_position()
@@ -129,7 +130,27 @@ function ball_collision() {
         ball_position.y_velocity = -ball_position.y_velocity //when ball going downwards it become positive
     }                                                        //so when hit the board, deduct with y_velocity
 }                                                            //positive become negative & and negative become positive 
-              
+
+function ball_collision_with_paddle() {
+    const ball_width_coordinate = ball_position.x_axis + ball.offsetWidth //by adding x_axis + width = rightmost coordinate
+    const ball_height_coordinate = ball_position.y_axis + ball.offsetHeight //by adding y_axis + height = bottommost coordinate 
+    const left_paddle_vertical = left_paddle.offsetTop + left_paddle.offsetHeight //by adding left_paddle.offsetTop + height = bottommost coordinate 
+    const right_paddle_vertical = right_paddle.offsetTop + right_paddle.offsetHeight //by adding right_paddle.offsetTop + height = bottommost coordiante
+    const right_boundary = 600 - right_paddle.offsetWidth // calculate the right paddle's left boundary (taking its width into account)
+    // check if ball collide with left paddle
+    if (ball_position.x_axis <= left_paddle.offsetWidth && //if ball's left side hit left paddle's right side within the width
+        ball_position.y_axis < left_paddle_vertical && //and ball y_axis is before left paddle vertical coordinate
+        ball_height_coordinate > left_paddle.offsetTop) { //and ball_height_coordinate is before paddle top coordinate
+            ball_position.x_velocity *= -1 //by multiplying minus 1, velocity will change from 5 to -5(move to the left)
+        }// all this rule is to make sure the ball bounce perfectly with the paddle, if one condition is removed, the ball will not bounce at some angle
+    // check if ball collide with right paddle
+    if (ball_width_coordinate >= right_boundary &&  //if ball's right side hit right paddle's left side within the width
+        ball_position.y_axis < right_paddle_vertical &&  //and ball's y_axis is before right paddle vertical coordinate
+        ball_height_coordinate > right_paddle.offsetTop ) { //and ball_height_coordinate is before paddle top coordinate
+            ball_position.x_velocity *= -1 //by miltiplying minus 1, velocity will change from -5 to 5(move to the right)
+        }
+}
+
 function render_ball_position() { //render ball position with x and y axis from the ball_position state
     ball.style.left = ball_position.x_axis + 'px'
     ball.style.top = ball_position.y_axis + 'px'
@@ -144,14 +165,14 @@ function move_paddle(key) {
 }
 
 function move_paddle_up() {
-    let current_position = right_paddle.offsetTop
+    let current_position = right_paddle.offsetTop //get current paddle position using offsetTop, to get only integer
     if (current_position > 0) { //cannot be lesser than 0, if not paddle will exit the board
-        right_paddle.style.top = (current_position - PADDLE_SPEED) + 'px';
+        right_paddle.style.top = (current_position - PADDLE_SPEED) + 'px'
     }
 }
 
 function move_paddle_down() {
-    let current_position = right_paddle.offsetTop //get current paddle position using offsetTop, to get only integer
+    let current_position = right_paddle.offsetTop 
     if (current_position + right_paddle.offsetHeight < 400) {  // 400 = board height, offsetHeight to get padding height in integer
         right_paddle.style.top = (current_position + PADDLE_SPEED) + 'px'
     } // if paddle current position added with paddle_speed(10) is greater than 400, it will not move
@@ -168,3 +189,6 @@ function AI_paddle_movement() {
         left_paddle.style.top = '340px' //paddle height - board height = 340
     }
 }
+
+
+    
